5f851766e702449033546a5d037683ad441e7d7f
Enlisar <bishtaaron2007@gmail.com>
2025-06-22 22:44:48 +0530

    started currency converter

--- src/Pages/CurrencyConv.jsx
+++ src/Pages/CurrencyConv.jsx
@@ -3,8 +3,96 @@ import React from "react";
 const Currency = () => {
   return (
     <>
-      <div className="bg-[#bc9060bb] rounded-xl p-6 shadow-xl w-[90%] max-w-[500px] mx-auto absolute text-center">
-        Currency Convertor in progress
+      <div className="bg-[#bc9060bb] rounded-xl p-5 shadow-xl w-[90%] max-w-[700px] mx-auto absolute flex flex-col gap-2 justify-center items-center ">
+        <div className="text-2xl mb-2 text-[#805d2d] font-semibold">
+          Currency Converter
+        </div>
+        <div className="flex flex-col sm:flex-row gap-6">
+          <div className="flex flex-row">
+            <input
+              type="text"
+              inputMode="numeric"
+              pattern="[0-9\s]{13,19}"
+              className="bg-[#8a6339] h-[40px] placeholder:text-[#e7c5a1] p-2 rounded text-[#e7c5a1] w-auto"
+              placeholder="From"
+            />
+            <select defaultValue="From" className="select w-[80px]">
+              <option disabled={true}>From</option>
+              <option value="AUD">AUD</option>
+              <option value="BGN">BGN</option>
+              <option value="BRL">BRL</option>
+              <option value="CAD">CAD</option>
+              <option value="CHF">CHF</option>
+              <option value="CNY">CNY</option>
+              <option value="CZK">CZK</option>
+              <option value="DKK">DKK</option>
+              <option value="EUR">EUR</option>
+              <option value="GBP">GBP</option>
+              <option value="HKD">HKD</option>
+              <option value="HUF">HUF</option>
+              <option value="IDR">IDR</option>
+              <option value="ILS">ILS</option>
+              <option value="INR">INR</option>
+              <option value="ISK">ISK</option>
+              <option value="JPY">JPY</option>
+              <option value="KRW">KRW</option>
+              <option value="MXN">MXN</option>
+              <option value="MYR">MYR</option>
+              <option value="NOK">NOK</option>
+              <option value="NZD">NZD</option>
+              <option value="PHP">PHP</option>
+              <option value="PLN">PLN</option>
+              <option value="RON">RON</option>
+              <option value="SEK">SEK</option>
+              <option value="SGD">SGD</option>
+              <option value="THB">THB</option>
+              <option value="TRY">TRY</option>
+              <option value="ZAR">ZAR</option>
+            </select>
+          </div>
+          <div className="flex flex-row">
+            <input
+              type="text"
+              inputMode="numeric"
+              pattern="[0-9\s]{13,19}"
+              className="bg-[#8a6339] h-[40px] placeholder:text-[#e7c5a1] p-2 rounded text-[#e7c5a1] w-auto"
+              placeholder="To"
+            />
+            <select defaultValue="To" className="select w-[80px]">
+              <option disabled={true}>To</option>
+              <option value="AUD">AUD</option>
+              <option value="BGN">BGN</option>
+              <option value="BRL">BRL</option>
+              <option value="CAD">CAD</option>
+              <option value="CHF">CHF</option>
+              <option value="CNY">CNY</option>
+              <option value="CZK">CZK</option>
+              <option value="DKK">DKK</option>
+              <option value="EUR">EUR</option>
+              <option value="GBP">GBP</option>
+              <option value="HKD">HKD</option>
+              <option value="HUF">HUF</option>
+              <option value="IDR">IDR</option>
+              <option value="ILS">ILS</option>
+              <option value="INR">INR</option>
+              <option value="ISK">ISK</option>
+              <option value="JPY">JPY</option>
+              <option value="KRW">KRW</option>
+              <option value="MXN">MXN</option>
+              <option value="MYR">MYR</option>
+              <option value="NOK">NOK</option>
+              <option value="NZD">NZD</option>
+              <option value="PHP">PHP</option>
+              <option value="PLN">PLN</option>
+              <option value="RON">RON</option>
+              <option value="SEK">SEK</option>
+              <option value="SGD">SGD</option>
+              <option value="THB">THB</option>
+              <option value="TRY">TRY</option>
+              <option value="ZAR">ZAR</option>
+            </select>
+          </div>
+        </div>
       </div>
     </>
   );
